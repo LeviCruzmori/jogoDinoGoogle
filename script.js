2ac36855d7d77b5ed52224324ee68ec1b7233bcc
LeviCruzmori <dsciencemori@gmail.com>
2020-10-05 08:52:37 -0300

    add evento pulo

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-
+// add dino no script
 const dino = document.querySelector('.dino');
 console.log(dino);
 
+// // identificando quando a tecla é pressionada
+// function handleKeyup(event){
+
+// }
+
+// crinado o pulo do dino
+document.addEventListener('keyup',() =>{
+    console.log('pressionou uma tecla');
+});
